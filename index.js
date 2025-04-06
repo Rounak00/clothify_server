0a9b53f25cff1ac5481029770fe551a610caf63d
Rounak00 <mukherjeerounak10@gmail.com>
2025-04-07 01:02:55 +0530

    Added the server

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: FRONTEND_URL,
+    origin: true,
     credentials: true,
   }));
 app.use(express.urlencoded({ extended: true }));
