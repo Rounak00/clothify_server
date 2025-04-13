e088521c8452cf54ee50ad5d24c6dc67d961b922
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2025-04-13 16:17:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: true,
+    origin: [FRONTEND_URL],
     credentials: true,
   }));
 app.use(express.urlencoded({ extended: true }));
